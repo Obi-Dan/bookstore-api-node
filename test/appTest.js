32af2806e038c2147d99257af28fd284952f2507
Daniel Hofferbert <danshoff@gmail.com>
2020-07-14 13:59:30 -0500

    Updates for initial code

--- test/appTest.js
+++ test/appTest.js
@@ -1,7 +1,9 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require ('../app.js');
+const { app, HOST, PORT } = require ('../app');
 const { expect } = require('chai');
+
+const BASE_URI = `http://${HOST}:${PORT}`;
 
 chai.use(chaiHttp);
 chai.should();
@@ -9,11 +11,12 @@ chai.should();
 describe('Bookstore', () => {
     describe('GET /', () => {
         it('Hello World', (done) => {
-            chai.request('http://0.0.0.0:8080')
+            chai.request(BASE_URI)
                 .get('/')
                 .end((err, res) => {
                     expect(err).to.be.null;
-                    expect(res.body).to.be.equal('Hello World');
+                    expect(res.status).to.be.eql(200);
+                    expect(res.body.message).to.be.equal('Hello World');
                     done();
                 });
         });
